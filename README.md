469106f1ff8bff48aaa5eb8bda5331cd8df06447
merypwl <pirschmaria@gmail.com>
2023-11-11 17:40:52 -0300

    agregué funcion en js y arreglé el alert del boton, corregi un error de z index en la nav responsiva y agregué el readme

---
+++ README.md
@@ -0,0 +1,34 @@
+README
+
+Alumna: María Pirsch
+Curso: C59 BA Multiplica - Programación Web Inicial - Front End Developer
+
+#¿De qué se trata esto que vas a ver acá?
+
+Mi papá, Hernán, es profesor en la Universidad de Luján y cuando le conté que estaba empezando este curso me dijo "Ay, me podrías ayudar con la web de las cátedras mías porque es medio desastrosa la que tengo ahora."
+Este proyecto es eso: una manera de darle una mano a una de las personas más generosas que conozco y que más hizo por mí a lo largo de toda mi vida.
+
+#¿Qué herramientas usé para crear esto?
+
+Utilicé HTML, CSS y JavaScript.
+También agregué un poquito de Bootstrap (unos íconos en la nav bar).
+
+#¿Mantuve el mismo diseño desde el primer sketch?
+
+Jajajaja, NO. Sufrió una y mil modificaciones. Moví la nav por todos lados, modifiqué las fotos, le agregué una animación, cambié el logo de lugar. ay que ser flexibles.
+
+#¿Cómo me sentí haciendo esto?
+
+Desafiada.
+A veces medio perdida.
+Estimulada.
+Concentrada.
+Pero sobre todo muy feliz.
+
+#Agradecimientos
+
+A Cintia, mi profesora, que tiene una paciencia de santa.
+A Pedro, mi amigo, que siempre estaba ahí para darme una mano cuando me trababa con el código.
+A mi viejo, que fue quien inspiró esto.
+
+
--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const inputNombre = document.getElementById("nombre")
+const inputApellido = document.getElementById("apellido")
+const inputEmail = document.getElementById("email")
+const textareaCatedra = document.getElementById("catedra")
+
+
+
+let nombre = ''
+let apellido = ''
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
@@ -11,7 +20,43 @@ cerrar.addEventListener("click", () => {
 })
 
 
+
 const btn = document.getElementById('boton')
 btn.addEventListener('click', () => {
-    alert('Gracias por suscribirte');
+    saludarUsuario(nombre,apellido)
 })
+
+
+inputNombre.addEventListener('change', () => {
+    if(inputNombre.value) {
+       inputNombre.style = "background-color: #D2E0FB;" 
+       nombre = inputNombre.value
+    }
+})
+
+inputApellido.addEventListener('change', () => {
+    if(inputApellido.value) {
+       inputApellido.style = "background-color: #D2E0FB;" 
+       apellido = inputApellido.value
+    }
+})
+
+inputEmail.addEventListener('change' , () => {
+    if(inputEmail.value) {
+        inputEmail.style = "background-color: #D2E0FB;"
+        email = inputEmail.value
+    }
+})
+
+textareaCatedra.addEventListener('change' , () => {
+    if(textareaCatedra.value) {
+        textareaCatedra.style = "background-color: #D2E0FB;"
+        catedra = textareaCatedra.value
+    }
+})
+
+
+
+function saludarUsuario(nombre,apellido) {
+    alert(`${nombre} ${apellido}, gracias por suscribirte.`)
+}
